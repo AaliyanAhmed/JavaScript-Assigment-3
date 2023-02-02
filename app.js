eb8b6ca8c4c98e2e1a82e8d1623fc1d3f7e98b29
Aaliyan Ahmed <121717147+AaliyanAhmed@users.noreply.github.com>
2023-02-02 01:17:54 -0800

    Java Script Work

---
+++ app.js
@@ -0,0 +1,168 @@
+// Question1
+// let num1 = 5;
+// let num2 = 10;
+// let sum = num1 + num2;
+// document.write("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+//Question2
+// let num1 = 5;
+// let num2 = 10;
+// let sum = num1 + num2;
+// let difference = num1 - num2;
+// let product = num1 * num2;
+// let quotient = num1 / num2;
+// let modulus = num1 % num2;
+// document.write("The sum of " + num1 + " and " + num2 + " is " + sum + "<br>");
+// document.write("The difference of " + num1 + " and " + num2 + " is " + difference + "<br>");
+// document.write("The product of " + num1 + " and " + num2 + " is " + product + "<br>");
+// document.write("The quotient of " + num1 + " and " + num2 + " is " + quotient + "<br>");
+// document.write("The modulus of " + num1 + " and " + num2 + " is " + modulus + "<br>");
+
+
+//Question3
+// let num;
+// document.write("Value after letiable declaration is: " + num + "<br>");
+// num = 5;
+// document.write("Initial value: " + num + "<br>");
+// num++;
+// document.write("Value after increment is: " + num + "<br>");
+// num += 7;
+// document.write("Value after addition is: " + num + "<br>");
+// num--;
+// document.write("Value after decrement is: " + num + "<br>");
+// let remainder = num % 3;
+// document.write("The remainder is: " + remainder + "<br>");
+
+//Question4
+// Cost of one movie ticket
+// let ticketPrice = 600;
+// let totalCost = ticketPrice * 5;
+// document.write("The total cost of buying 5 tickets is: " + totalCost + " PKR");
+
+//Question5
+// document.write("<h1>Tables of 4</h1><h2></br>4x1=4</br>4x2=8</br>4x3=12</br>4x4=16</br>4x5=20</br>4x6=24</br>4x7=28</br>4x8=32</br>4x9=36</br>4x10=40</h2>")
+
+// ************Question 6************
+// Celsius to Fahrenheit conversion
+// let celsius_temperature = 25;
+// let fahrenheit_temperature = (celsius_temperature * 9/5) + 32;
+// document.write(`${celsius_temperature}°C is ${fahrenheit_temperature}°F </br>`);
+// fahrenheit_temperature = 77;
+// celsius_temperature = (fahrenheit_temperature - 32) * 5/9;
+// document.write(`${fahrenheit_temperature}°F is ${celsius_temperature}°C`);
+
+//Question7
+// Store letiables
+// let item1Price = 20;
+// let item2Price = 15;
+// let item1Quantity = 3;
+// let item2Quantity = 2;
+// let shippingCharges = 5;
+// let totalCost = (item1Price * item1Quantity) + (item2Price * item2Quantity) + shippingCharges;
+// document.write(`
+//   <h1>Receipt</h1>
+//   <p>Item 1: $${item1Price} x ${item1Quantity} = $${item1Price * item1Quantity}</p>
+//   <p>Item 2: $${item2Price} x ${item2Quantity} = $${item2Price * item2Quantity}</p>
+//   <p>Shipping Charges: $${shippingCharges}</p>
+//   <hr>
+//   <h3>Total Cost: $${totalCost}</h3>
+// `);
+
+//Question8
+// Store marks in letiables
+// let totalMarks = 500;
+// let marksObtained = 450;
+// let percentage = (marksObtained / totalMarks) * 100;
+// document.write(`
+//   <h1>Result</h1>
+//   <p>Total Marks: ${totalMarks}</p>
+//   <p>Marks Obtained: ${marksObtained}</p>
+//   <p>Percentage: ${percentage}%</p>
+// `);
+
+// Question 9
+// document.write("<h1>Currency in PKR</h1>")
+// let usd = 10;
+// let sar = 25;
+// let usdToPkr = 267.50;
+// let sarToPkr = 71.29;
+// let pkr = (usd * usdToPkr) + (sar * sarToPkr);
+// document.write(`<h2>Total currency in Pakistani Rupees: ${pkr}</h2>`);
+
+//Question10
+// let num = 10;
+// let result = ((num + 5) * 10) / 2;
+// document.write(result);
+
+//Question11
+// let currentYear = 2023;
+// let birthYear = 2004;
+// let age1 = currentYear - birthYear;
+// let age2 = age1 - 1;
+// document.write(`<h1>They are either ${age2} or ${age1} years old.</h1>`);
+
+// Question12
+// document.write("<h1> The Geometric</h1>")
+// let radius =  5 ;
+// document.write("<h2> Radius of circle = " , radius ,"</h2>")
+// let circumference = 2 * Math.PI * radius;
+// let area = Math.PI * (radius ** 2);
+// document.write(`<h2>The circumference is =  ${circumference.toFixed(2)}</h2>`);
+// document.write(`<h2>The area is =  ${area.toFixed(2)}</h2>`);
+
+// Question13
+// Store favorite snack
+// let snack = "chocolate bars";
+// let currentAge = 30;
+// let maxAge = 85;
+// let amountPerDay = 2;
+// let totalAmount = (maxAge - currentAge) * 365 * amountPerDay;
+// document.write(`<h1>You will need ${totalAmount} ${snack} to last you until the ripe old age of ${maxAge}.</h1>`);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Chapter 6-9~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Qustion no1
+// document.write("<h1>Result</h1>");
+// let a= 10;
+// document.write('The value of is a: ${a}</br>');
+// document.write('.................................</br>');
+// ++a
+// document.write('The value of ++a is:${a}</br>')
+// --a
+// document.write('The value of --a is:${a}</br>')
+// a--
+// document.write('The value of a-- is:${a}</br>')
+
+// Question2
+// let a = 2, b = 1;
+// document.write('a is ${a}</br>')
+// document.write('b is ${b}</br>')
+// let result = --a - --b + ++b + b--;
+// document.write('result is${result}</br>')
+
+// Quration 3
+// let name = prompt("Enter your name: ");
+// alert("Hello, " + name + "! Welcome!");
+
+// Question4
+// document.write("<h1>Tables of 5</h1><h2></br>5x1=5</br>5x2=10</br>5x3=15</br>5x4=20</br>5x5=25</br>5x6=30</br>5x7=35</br>5x8=40</br>5x9=45</br>5x10=50</h2>")
+
+// Question5
+// let subject1 = prompt("Enter first subject name: ");
+// let subject2 = prompt("Enter second subject name: ");
+// let subject3 = prompt("Enter third subject name: ");
+// let totalMarks = 100;
+// let obtainedMarks1 = parseInt(prompt("Enter obtained marks for " + subject1 + ": "));
+// let obtainedMarks2 = parseInt(prompt("Enter obtained marks for " + subject2 + ": "));
+// let obtainedMarks3 = parseInt(prompt("Enter obtained marks for " + subject3 + ": "));
+// let totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+// let percentage = (totalObtainedMarks / (totalMarks * 3)) * 100;
+// document.write("<table><tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>");
+// document.write("<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks1 + "</td></tr>");
+// document.write("<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks2 + "</td></tr>");
+// document.write("<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks3 + "</td></tr>");
+// document.write("<tr><td><b>Total</b></td><td><b>" + (totalMarks * 3) + "</b></td><td><b>" + totalObtainedMarks + "</b></td></tr>");
+// document.write("<tr><td><b>Percentage</b></td><td colspan='2'><b>" + percentage + "%</b></td></tr></table>");
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
